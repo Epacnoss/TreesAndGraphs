9b6a04e2277d2e89cd4d7dfe141453b47154c470
Jack Maguire <jackmaguire1234@gmail.com>
2019-04-24 18:07:08 +0100

    kiddies and parents

--- src/program.java
+++ src/program.java
@@ -5,8 +5,26 @@ public class program {
 
     public static void main (String[] args)
     {
-        Node n = new Node("Hi fam...");
-        System.out.println(n);
+        List<Node> nodes = new ArrayList<>();
+
+        String temp = "";
+        for (int i = 0; i < 100; i++) {
+            temp += i;
+            if(i == 0)
+                nodes.add(new Node(temp));
+            else
+                nodes.add(new Node(nodes.get(i - 1), temp));
+
+        }
+
+        for (int i = 1; i < nodes.size() - 1; i++)
+        {
+            nodes.get(i).addChild(nodes.get(i - 1));
+        }
+
+        for (int i = 0; i < nodes.size(); i++) {
+            System.out.println(nodes.get(i));
+        }
     }
 
 
