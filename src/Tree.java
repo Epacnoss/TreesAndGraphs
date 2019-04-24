9069ebae7c9d8a7c3fc6958fe925f92a85468bab
Jack Maguire <jackmaguire1234@gmail.com>
2019-04-24 18:17:20 +0100

    Tree

---
+++ src/Tree.java
@@ -0,0 +1,20 @@
+import java.util.StringJoiner;
+
+public class Tree {
+
+    private Node root = null;
+
+    protected Node getRoot() {
+        return root;
+    }
+
+    public Boolean isEmpty ()
+    {
+        return root == null;
+    }
+
+    public void print ()
+    {
+
+    }
+}
