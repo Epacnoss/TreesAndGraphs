e48d981a098e04e5494d942b3c7862bc609b9f66
Jack Maguire <jackmaguire1234@gmail.com>
2019-04-24 18:02:55 +0100

    getting it to start

--- src/Node.java
+++ src/Node.java
@@ -1,13 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringJoiner;
+
 public class Node {
 
     private Node parent;
-    private Node[] children;
+    private List<Node> children;
 
     private String content;
 
-    public Node(Node parent, Node[] children, String content) {
-        this.parent = parent;
-        this.children = children;
+    public Node(String content) {
         this.content = content;
+        children = new ArrayList<>();
+    }
+
+    public Node(Node parent, String content)
+    {
+        this.parent = parent;
+        this.content = content;
+        children = new ArrayList<>();
+    }
+
+    public void setParent (Node parent)
+    {
+        this.parent = parent;
+    }
+
+    public void addChild (Node child)
+    {
+        children.add(child);
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Node.class.getSimpleName() + "[", "]")
+                .add("content='" + content + "'")
+                .toString();
     }
 }
---
+++ src/program.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class program {
+
+    public static void main (String[] args)
+    {
+        Node n = new Node("Hi fam...");
+        System.out.println(n);
+    }
+
+
+}
