df1f858acc69f094dceb832f04bb5fdf8aa5d03d
Jack Maguire <jackmaguire1234@gmail.com>
2019-04-24 18:40:42 +0100

    death of my baby

--- src/program.java
+++ src/program.java
@@ -5,27 +5,28 @@ public class program {
 
     public static void main (String[] args)
     {
-        List<Node> nodes = new ArrayList<>();
+//        List<Node> nodes = new ArrayList<>();
+//
+//        //region Nodes init
+//        String temp = "";
+//        for (int i = 0; i < 100; i++) {
+//            temp += i;
+//            if(i == 0)
+//                nodes.add(new Node(temp));
+//            else
+//                nodes.add(new Node(nodes.get(i - 1), temp));
+//
+//        }
+//        for (int i = 1; i < nodes.size() - 1; i++)
+//        {
+//            nodes.get(i).addChild(nodes.get(i - 1));
+//        }
+//        //endregion
+//
+//        for (int i = 0; i < nodes.size(); i++) {
+//            System.out.println(nodes.get(i));
+//        }
 
-        //region Nodes init
-        String temp = "";
-        for (int i = 0; i < 100; i++) {
-            temp += i;
-            if(i == 0)
-                nodes.add(new Node(temp));
-            else
-                nodes.add(new Node(nodes.get(i - 1), temp));
-
-        }
-        for (int i = 1; i < nodes.size() - 1; i++)
-        {
-            nodes.get(i).addChild(nodes.get(i - 1));
-        }
-        //endregion
-
-        for (int i = 0; i < nodes.size(); i++) {
-            System.out.println(nodes.get(i));
-        }
     }
 
 
